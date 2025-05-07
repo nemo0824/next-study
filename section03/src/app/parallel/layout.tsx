71bda8608d708ad3ed92c405216aa9d4f5340222
임재원 <nemo0824@naver.com>
2025-05-07 11:13:25 +0900

    feature: parallel

---
+++ section03/src/app/parallel/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+
+export default function Layout({ children, sidebar }: { children: ReactNode, sidebar: ReactNode }) {
+  return (
+    <div>
+      {sidebar}
+      {children}
+    </div>
+  )
+}
