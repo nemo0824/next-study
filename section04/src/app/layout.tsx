bdc32d44b88493f31da88e07ea86b268b1b5fc22
임재원 <nemo0824@naver.com>
2025-09-01 00:33:28 +0900

    chore: reset section 04

---
+++ section04/src/app/layout.tsx
@@ -0,0 +1,11 @@
+import "./globals.css";
+
+export default function RootLayout({}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body></body>
+    </html>
+  );
+}
