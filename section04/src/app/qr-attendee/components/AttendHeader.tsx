b3c4f3732c9840a54af6629cb28e45bee36770da
임재원 <nemo0824@naver.com>
2025-09-15 16:24:45 +0900

    chore: 조치 사항 작성

--- section04/src/app/qr-attendee/components/AttendHeader.tsx
+++ section04/src/app/qr-attendee/components/AttendHeader.tsx
@@ -20,6 +20,7 @@ export const AttendHeader = ({
   const { title, icon } = getHeaderConfig();
   return (
     <header className="h-15 border border-[#D8D8D8] flex items-center relative">
+      {/* 뒤로가기 아이콘 각각 상황에 필요할지 고민 및 이동로직 삽입 필요 (true/false) 가능하면 변경 */}
       <div className="absolute left-4 cursor-pointer">{icon}</div>
       <h3 className="font-bold flex-1 text-center">{title}</h3>
     </header>
